4582fcffb3de31513a41f65a926ddcadda4e3dcb
epic-website-lol <eli.taylor@cmsdk12.org>
2025-09-05 15:16:11 -0400

    Create echastonechatbot.py

---
+++ echastonechatbot.py
@@ -0,0 +1,20 @@
+print("Welcome to my chatbot named, Echastone!")
+var = input("Hi how are you?")
+if var == "great":
+    print("nice, glad to hear that!")
+if var == "ok":
+    print("nice, glad to hear that!")
+if var == "alright":
+    print("nice, glad to hear that!")
+if var == "good":
+    print("nice, glad to hear that!")
+if var == "bad":
+    print("well dang, hope tomorrows better!")
+if var == "well":
+    print("nice, glad to hear that!")
+if var == "terrible":
+    print("oh, well i hope you come across better times!")
+if var == "horrible":
+    print("oh, well i hope you come across better times!")
+if var == "sad":
+    print("oh, well i hope you come across better times!")
