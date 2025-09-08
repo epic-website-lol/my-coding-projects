d19781e7661e7a21c80d7f63b628b2bff5839b42
epic-website-lol <eli.taylor@cmsdk12.org>
2025-09-08 09:18:06 -0400

    Update randomnum.py

--- randomnum.py
+++ randomnum.py
@@ -1,6 +1,6 @@
 # made by Eli Taylor in 2025
-import random 
-x = "1"
-while x == "1":
-  input("Press enter to generate a random number... ")
-  print(random.random())
+import random # imports the "random" module into the project
+x = "1" # creates a variable with the value, "1"
+while x == "1": # repeats everything below as long as x is equal to, "1"
+  input("Press enter to generate a random number... ") # detects if input is placed then does the below.
+  print(random.random()) # prints a random number
