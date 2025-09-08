d6096d91345e28d9e776478f740369d9c1c486de
epic-website-lol <eli.taylor@cmsdk12.org>
2025-09-08 09:21:48 -0400

    Update guesser.py

--- guesser.py
+++ guesser.py
@@ -1,5 +1,5 @@
 # made by Eli Taylor, in 2025.
-print("Welcome to a simple guessing game!")
-text = input("Guess the correct number! (1-10) > ")
-if num == "7":
-  print("You guessed it! Congrats!")
+print("Welcome to a simple guessing game!") # displays text on the screen
+text = input("Guess the correct number! (1-10) > ") # detects what you type in and sends it to the value of the "text" variable
+if text == "7": # detects if the variable that was previously set equals 7
+  print("You guessed it! Congrats!") # if it is equal then it displays text on the screen
