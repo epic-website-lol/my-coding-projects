0bc104e3a1a9e5fdd832a318b829c4e2251aaf0f
epic-website-lol <eli.taylor@cmsdk12.org>
2025-09-09 15:43:18 -0400

    Delete guesser2.py

--- guesser2.py
+++
@@ -1,9 +0,0 @@
-import random
-import time
-var = random.random(1, 10)
-print("Pick a number 1-10!")
-if num == var:
-  print("Brilliant!")
-  time.sleep(10)
-else:
-  print("Wrong! the answer was," + var)
